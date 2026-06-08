9bc5e5eecb0509471fe68e553039d8ae4220ddf8
MagmaVRC <magma.mc@magma-mc.net>
2026-06-08 07:51:39 +0100

    Bump version to 0.3.0

--- src/core/AppVersion.h
+++ src/core/AppVersion.h
@@ -2,7 +2,7 @@
 
 namespace core {
 
-constexpr const char* kAppVersion = "0.2.0";
+constexpr const char* kAppVersion = "0.3.0";
 constexpr const char* kAppRepo = "MagmaVRC/S2ModManager";
 constexpr const char* kAppReleasesUrl = "https://github.com/MagmaVRC/S2ModManager/releases";
 
